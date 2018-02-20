0a41e9cc8e1cd422a25a13e6a2348917651ab0d7
Néstor Bermúdez <nestor.bermudez@agilityfeat.com>
2018-02-20 12:49:33 -0600

    Add methods for the different clustering methods

--- assignment_1/clustering.py
+++ assignment_1/clustering.py
@@ -6,3 +6,13 @@ class PhraseClustering:
     def run(self, X, n_clusters, distance):
         cluster = SphericalKMeans(n_clusters)
         return cluster.fit_predict(X)
+
+
+def spherical_clustering(X, n_clusters):
+    cluster = SphericalKMeans(n_clusters)
+    return cluster.fit_predict(X)
+
+
+def euclidean_clustering(X, n_clusters):
+    cluster = KMeans(n_clusters=n_clusters)
+    return cluster.fit_predict(X)
